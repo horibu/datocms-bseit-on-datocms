40a750800d70c9de4da21a018f044ad2c899ee03
HB <hb@horibuful.de>
2020-06-10 13:19:23 +0200

    test

---
+++ pages/test/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Test() {
+  return (
+    <Layout>
+      <Head>
+        <title>Next.js Blog Example with {CMS_NAME}</title>
+      </Head>
+      <Container>
+        <h2>TEST</h2>
+      </Container>
+    </Layout>
+  );
+}
